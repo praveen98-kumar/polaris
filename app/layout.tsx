f6277a698a489f0b0117bc961db0970008f30f03
Praveen Kumar Koram <pkkoram98@gmail.com>
2026-01-06 09:47:16 +0530

    feat: add dark mode support

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
@@ -30,8 +31,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+      >
         {children}
         <Toaster />
+      </ThemeProvider>
       </body>
     </html>
   );
